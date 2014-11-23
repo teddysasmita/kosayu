d82648a53bf7ee33c5ee66f5d4eff7e8e6cade46
teddysasmita <teddie.sasmita@gmail.com>
2014-11-23 14:40:17 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/salespos/views/salesposreport/viewsales.php
+++ protected/modules/salespos/views/salesposreport/viewsales.php
@@ -9,11 +9,7 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'Tambah Data', 'url'=>array('create')),
-	array('label'=>'Ubah Data', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Hapus Data', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Pencarian Data', 'url'=>array('admin')),
-      array('label'=>'Sejarah', 'url'=>array('history', 'id'=>$model->id)),
+	
 );
 ?>
 
