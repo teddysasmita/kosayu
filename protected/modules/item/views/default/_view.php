d1f5993366e40dc512b90cebe59b0386f521da6d
teddysasmita <teddie.sasmita@gmail.com>
2014-11-23 10:20:55 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/item/views/default/_view.php
+++ protected/modules/item/views/default/_view.php
@@ -5,12 +5,8 @@
 
 <div class="view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
-
 	<b><?php echo CHtml::encode($data->getAttributeLabel('code')); ?>:</b>
-	<?php echo CHtml::encode($data->code); ?>
+	<?php echo CHtml::link(CHtml::encode($data->code), array('view', 'id'=>$data->id)); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('type')); ?>:</b>
