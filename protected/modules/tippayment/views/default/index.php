83abeee9e8c951000a93f81d8cab6421af1475c4
teddysasmita <teddie.sasmita@gmail.com>
2014-11-24 19:59:18 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/tippayment/views/default/index.php
+++ protected/modules/tippayment/views/default/index.php
@@ -11,7 +11,6 @@ $this->menu=array(
    array('label'=>'Tambah Data', 'url'=>array('create')),
    array('label'=>'Pencarian Data', 'url'=>array('admin')),
    array('label'=>'Data yang telah dihapus', 'url'=>array('deleted')),
-	array('label'=>'Nomor Seri Barang Masuk', 'url'=>array('serial')),
 );
 ?>
 
