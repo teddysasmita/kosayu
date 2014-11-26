f7e0cfb56ec2dfbb1eccae63eb806225d6a4685d
teddysasmita <teddie.sasmita@gmail.com>
2014-11-26 14:31:57 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/salesvouchers/controllers/DefaultController.php
+++ protected/modules/salesvouchers/controllers/DefaultController.php
@@ -7,7 +7,7 @@ class DefaultController extends Controller
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
 	public $layout='//layouts/column2';
-    public $formid='AB3';
+    public $formid='AB15';
 	public $tracker;
 	public $state;
 
