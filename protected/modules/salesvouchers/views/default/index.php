d8f43351dec67a67ec393f26e5dd65d6ac9ab6ff
teddysasmita <teddie.sasmita@gmail.com>
2014-11-26 14:35:49 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/salesvouchers/views/default/index.php
+++ protected/modules/salesvouchers/views/default/index.php
@@ -11,7 +11,6 @@ $this->menu=array(
 	array('label'=>'Tambah Data', 'url'=>array('create')),
 	array('label'=>'Pencarian Data', 'url'=>array('admin')),
    array('label'=>'Data yang telah terhapus', 'url'=>array('deleted')),
-		array('label'=>'Export Nama ke Xcl', 'url'=>array('export2xcl')),
 );
 ?>
 
