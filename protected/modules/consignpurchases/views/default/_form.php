1b23b3293b77efff343356522956a663e0908fc4
teddysasmita <teddie.sasmita@gmail.com>
2014-12-01 13:06:45 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/consignpurchases/views/default/_form.php
+++ protected/modules/consignpurchases/views/default/_form.php
@@ -95,12 +95,12 @@ EOS;
 		<?php echo $form->labelEx($model,'idsupplier'); ?>
 		<?php 
                $suppliers=Yii::app()->db->createCommand()
-                  ->select("id,firstname,lastname")
+                  ->select("id,code,firstname,lastname")
                   ->from("suppliers")
                   ->order("firstname, lastname")   
                   ->queryAll();
                foreach($suppliers as $row) {
-                  $suppliername[]=$row['firstname'].' '.$row['lastname'];
+                  $suppliername[]=$row['code'].'-'.$row['firstname'].' '.$row['lastname'];
                }
                $this->widget("zii.widgets.jui.CJuiAutoComplete", array(
                    'name'=>'Consignpurchases_suppliername',
