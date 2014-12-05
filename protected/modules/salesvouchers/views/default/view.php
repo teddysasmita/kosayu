02afeff965e151cdea037293a7e5c3775b83a975
teddysasmita <teddie.sasmita@gmail.com>
2014-12-05 16:44:25 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/salesvouchers/views/default/view.php
+++ protected/modules/salesvouchers/views/default/view.php
@@ -22,7 +22,7 @@ $this->menu=array(
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
-		'id',
+		//'id',
 		'regnum',
 		array(
                    'name'=>'amount',
