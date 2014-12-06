88bec46679318b0b8391beb5b02cd781f9cc31cf
teddysasmita <teddie.sasmita@gmail.com>
2014-12-06 09:10:43 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/salespos/controllers/SalesposreportController.php
+++ protected/modules/salespos/controllers/SalesposreportController.php
@@ -309,13 +309,13 @@ EOS;
 				}
 			}
 						
-			$xl->getActiveSheet()->setTitle('Laporan Penjualan');
+			/*$xl->getActiveSheet()->setTitle('Laporan Penjualan');
 			$xl->setActiveSheetIndex(0);
 			header('Content-Type: application/pdf');
 			header('Content-Disposition: attachment;filename="sales-report-'.idmaker::getDateTime().'.xlsx"');
 			header('Cache-Control: max-age=0');
 			$xlWriter = PHPExcel_IOFactory::createWriter($xl, 'Excel2007');
-			$xlWriter->save('php://output');
+			$xlWriter->save('php://output');*/
 		} else {
 			throw new CHttpException(404,'You have no authorization for this operation.');
 		};
