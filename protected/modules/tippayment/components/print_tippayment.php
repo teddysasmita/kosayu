9f39e259087607b565427f8ebf8b5fd7e656303a
teddysasmita <teddie.sasmita@gmail.com>
2015-01-02 18:09:22 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/tippayment/components/print_tippayment.php
+++ protected/modules/tippayment/components/print_tippayment.php
@@ -103,7 +103,7 @@ class MYPDF extends TCPDF {
 		$this->Cell(85,1,'','B',1);
 		
 		$this->setX($this->leftmargin);
-		$this->SetFontSize(10);
+		$this->SetFontSize(11);
 		$this->Cell(45, 5, 'Total :', 0, 0, 'R'); 
 		$this->Cell(40, 5, number_format($this->data->amount), 0, 1, 'R');
 	}
