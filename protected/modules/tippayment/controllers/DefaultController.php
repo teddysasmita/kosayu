acaca3aa5a2e7ff6c97467c17dbebf211809db65
teddysasmita <teddie.sasmita@gmail.com>
2015-01-03 11:58:15 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/tippayment/controllers/DefaultController.php
+++ protected/modules/tippayment/controllers/DefaultController.php
@@ -966,7 +966,7 @@ EOS;
     		$detailmodel=$this->loadDetails($id);
 			$detailmodel2=$this->loadDetails2($id);
 			
-			$this->renderFile('printtext.php', 
+			$this->renderFile('application.modules.tippayment.view.default.printtext', 
 				array('data'=>$model, 'detaildata'=>$detailmodel, 
 					'detaildata2'=>$detailmodel2)
 			);
