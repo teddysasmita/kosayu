5f897ca93021ecbf3b7f950acd4a7d12d0e5922e
teddysasmita <teddie.sasmita@gmail.com>
2015-01-04 19:10:04 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/stockadmin/views/default/index.php
+++ protected/modules/stockadmin/views/default/index.php
@@ -13,13 +13,6 @@ $this->menu=array(
 
 <h1><?php echo "Administrasi Persediaan (Stok)" ?></h1>
 
-<h3><?php echo CHtml::link('Berdasarkan Kuantitas dan Lokasi', 
+<h3><?php echo CHtml::link('Berdasarkan Stok', 
 		Yii::app()->createUrl('stockadmin/default/quantity'))?></h3> 
-<h3><?php echo CHtml::link('Berdasarkan Nomor Seri dan Lokasi', 
-		Yii::app()->createUrl('stockadmin/default/serial'))?></h3>
-<h3><?php echo CHtml::link('Kartu Stok berdasarkan Nama Barang dan Gudang', 
-		Yii::app()->createUrl('stockadmin/default/flow'))?></h3>
-<h3><?php echo CHtml::link('Daftar Stok Error', 
-		Yii::app()->createUrl('stockadmin/default/indexError'))?></h3>
-<h3><?php echo CHtml::link('Lacak Nomor Seri', 
-		Yii::app()->createUrl('stockadmin/default/trace'))?></h3>
+
