b64192590248bf129a68b40d9cb2d58dd58f0de0
teddysasmita <teddie.sasmita@gmail.com>
2015-02-01 11:57:06 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/purchasespayment/views/detailpurchasespayments3/_form.php
+++ protected/modules/purchasespayment/views/detailpurchasespayments3/_form.php
@@ -51,7 +51,6 @@ EOS;
          echo $form->hiddenField($model,'idatetime');
          echo $form->hiddenField($model,'userlog');
          echo $form->hiddenField($model,'datetimelog');
-         print_r(Yii::app()->session['Purchasespayments']);
         ?>
 
 	
