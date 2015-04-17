980f956a9ae0d069bd1a475c8f6a27518a98caa8
teddysasmita <teddie.sasmita@gmail.com>
2015-04-17 09:04:08 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/item/views/default/_view.php
+++ protected/modules/item/views/default/_view.php
@@ -5,20 +5,8 @@
 
 <div class="view">
 
-	<b><?php //echo CHtml::encode($data->getAttributeLabel('code')); ?></b>
-	<?php //echo CHtml::link(CHtml::encode($data->code), array('view', 'id'=>$data->id)); ?>
-	<br />
-
-	<b><?php //echo CHtml::encode($data->getAttributeLabel('type')); ?></b>
-	<?php //echo CHtml::encode(lookup::TypeToName($data->type)); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+		<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
 	<?php echo CHtml::link(CHtml::encode($data->name), array('view', 'id'=>$data->id)); ?>
-	<br />
-
-	<b><?php //echo CHtml::encode($data->getAttributeLabel('brand')); ?></b>
-	<?php //echo CHtml::encode($data->brand); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('picture')); ?>:</b>
