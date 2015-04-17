02007505fd01419f756406b442deac575a9ed76c
teddysasmita <teddie.sasmita@gmail.com>
2015-04-17 09:24:38 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/item/views/default/create.php
+++ protected/modules/item/views/default/create.php
@@ -3,7 +3,7 @@
 /* @var $model Items */
 
 $this->breadcrumbs=array(
-   'Proses'=>array('/site/proses'),
+   'Master Data'=>array('/site/masterdata'),
 	'Daftar'=>array('index'),
 	'Tambah Data',
 );
