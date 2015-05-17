410525f10d0d252b37e294016ab2cf17e486732d
teddysasmita <teddie.sasmita@gmail.com>
2015-05-17 09:05:26 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/paysalaries/views/default/_form.php
+++ protected/modules/paysalaries/views/default/_form.php
@@ -103,9 +103,9 @@ EOS;
 	<div class="row">
 		<?php echo $form->labelEx($model,'pmonth'); ?>
 		<?php
-			$pmonth = array(1=>'Januari', 2=>'Februari', 3=>'Maret', 4=>'April',
-				5=>'Mei', 6=>'Juni', 7=>'Juli', 8=>'Agustus', 9=>'September',
-				10=>'Oktober', 11=>'Nopember', 12=>'Desember'
+			$pmonth = array('1'=>'Januari', '2'=>'Februari', '3'=>'Maret', '4'=>'April',
+				'5'=>'Mei', '6'=>'Juni', '7'=>'Juli', '8'=>'Agustus', '9'=>'September',
+				'10'=>'Oktober', '11'=>'Nopember', '12'=>'Desember'
 			);
 			echo $form->dropDownList($model, 'pmonth', $pmonth); ?>
 		<?php echo $form->error($model,'pmonth'); ?>
