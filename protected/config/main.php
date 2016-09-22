f9cc22e07d92c45c3e851a3f197249fe05e48ea4
Teddy <Teddy@tedlaptop>
2016-09-22 09:35:45 +0700

    Signed-off-by: Teddy <Teddy@tedlaptop>

--- protected/config/main.php
+++ protected/config/main.php
@@ -24,7 +24,7 @@ return array(
 	
 		/*'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'test',
+			'password'=>'master',
                         
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
@@ -126,7 +126,7 @@ return array(
                'connectionString' => 'mysql:host=localhost;dbname=kosayu',
                'emulatePrepare' => true,
                'username' => 'root',
-               'password' => 'test',
+               'password' => 'master',
                'charset' => 'utf8',
 
              ),
@@ -136,7 +136,7 @@ return array(
                'connectionString' => 'mysql:host=localhost;dbname=kosayu-track',
                'emulatePrepare' => true,
                'username' => 'root',
-               'password' => 'test',
+               'password' => 'master',
                'charset' => 'utf8',
 
              ),
@@ -146,7 +146,7 @@ return array(
                'connectionString' => 'mysql:host=localhost;dbname=kosayu-auth',
                'emulatePrepare' => true,
                'username' => 'root',
-               'password'=>'test',
+               'password'=>'master',
                'charset' => 'utf8',
 
              ),
