ae9333fa652b682b87012fa1d566678af099ffb1
Teddy <Teddy@tedlaptop>
2016-09-26 05:11:39 +0700

    Change-Id: I569b4e2a0cd7050848e630197d0aca73ab954eef Signed-off-by: Teddy <Teddy@tedlaptop>

--- protected/modules/guides/views/default/printreport1.php
+++ protected/modules/guides/views/default/printreport1.php
@@ -4,83 +4,10 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Aktivitas Guide</title>
 <style>
-	@page { size: <?php echo $model->paperwidth; ?>mm <?php echo $model->paperheight; ?>mm;
-		margin-bottom: <?php echo $model->paperbotm; ?>mm;
-		margin-top: <?php echo $model->paperbotm; ?>mm; 
-		margin-right: <?php echo $model->papersidem; ?>mm;
-		margin-left: <?php echo $model->papersidem+10; ?>mm;
+	@page { size: 215mm 280mm;
+		margin: 10mm;
 		page-break-before: 5mm;
-	}
-	
-	.guidetable { 
-		/*margin-left: <?php echo $model->paperwidth - ( 3 * $model->papersidem) - $model->labelwidth - (2 * $model->labelsidem); ?>mm;*/ 	
-		border: none;
-	}
-	
-	.guidetablerow { 
-		height: <?php echo $model->labelheight; ?>mm;
-		margin: <?php echo $model->labelbotm; ?>mm; 
-	}
-	.guidetablecol { 
-		width: <?php echo $model->labelwidth; ?>mm;
-		margin: <?php echo $model->labelsidem; ?>mm;
-		 
-	}
-	.guidetablecell {  
-		border: solid thin;
-		-webkit-print-color-adjust: exact;
-	 }
-	 .cardcontent {
-	 	background-color: yellow;
-	 	height: <?php echo $model->labelheight-4; ?>mm;
-	 	width: <?php echo $model->labelwidth-4; ?>mm;
-	 	margin-top: <?php echo $model->labelbotm; ?>mm;
-	 	margin-bottom: <?php echo $model->labelbotm; ?>mm;
-	 	margin-right: <?php echo $model->labelsidem; ?>mm;
-	 	margin-left: <?php echo $model->labelsidem; ?>mm;
-	 	float: left;
-	 	padding: 2mm;
-	 	border: solid thin;
-	 	page-break-inside: avoid;
-	 }
-	 .emptycontent {
-	 	height: <?php echo $model->labelheight-2; ?>mm;
-	 	width: <?php echo $model->labelwidth-2; ?>mm;
-	 }
-	 
-	 .infofield {
-	 	margin: 10mm; width: 25mm;
-	 	font-family: monospace;
-	 	font-size: 10pt;
-	 }
-	 .infovalue {
-	 	margin: 10mm; width: 75mm;
-	 	font-family: serif;
-	 	font-size: 11pt;
-	 	background-color: yellow;
-	 	font-weight: bold;
-	 }
-	 .infocolon {
-	 	background-color: yellow;
-	 }
-	 .cardtitle {
-	 	text-align: center;
-	 	height: 10mm;
-	 }
-	 .barcodeinfo {
-	 	text-align: center;
-	 	/*padding-top: 4mm;*/
-	 }
-	 .addressinfo {
-	 	font-family: sans-serif;
-	 	font-size: 8pt;
-	 	text-align: center;
-	 }
-	 .cardtable {
-	 	border-collapse: collapse;
-	 	background-color: yellow;
-	 }
-	 
+	} 
 </style>
 </head>
 <body>
