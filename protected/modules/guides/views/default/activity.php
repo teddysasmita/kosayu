7c58b819f7311625ce86ac44bca41c38c63560bd
teddys <teddie.sasmita@gmail.com>
2016-09-28 11:02:54 +0700

    Signed-off-by: teddys <teddie.sasmita@gmail.com>

---
+++ protected/modules/guides/views/default/activity.php
@@ -0,0 +1,65 @@
+<?php
+/* @var $this GuidesController */
+/* @var $model Guides */
+
+$this->breadcrumbs=array(
+   'Master Data'=>array('/site/masterdata'),
+	'Daftar'=>array('index'),
+	'Lihat Data'=>array('view', ['id'=>$model->id]),
+	'Aktifitas'
+);
+
+$this->menu=array(
+	//array('label'=>'Daftar Pelanggan', 'url'=>array('index')),
+	array('label'=>'Cetak', 'url'=>array('printActivity')),
+);
+?>
+
+<h1>Pemandu / Guide</h1>
+
+<div class='form'>
+
+<?php 
+	CHtml::beginForm('activity', 'get');
+?>
+
+<div class="row">
+	<?php 
+	echo CHtml::label('Tanggal Awal', 'startdate');
+	$this->widget('zii.widgets.jui.CJuiDatePicker',array(
+			'name'=>'startdate',
+			// additional javascript options for the date picker plugin
+			'options'=>array(
+				'showAnim'=>'fold',
+			),
+			'htmlOptions'=>array(
+					'id'=>'startdate',
+					'style'=>'height:20px;'
+			),
+			'value'=>$startdate,
+	));
+	?>
+</div>
+
+<div class="row">
+	<?php 
+	echo CHtml::label('Tanggal Akhir', 'startdate');
+	$this->widget('zii.widgets.jui.CJuiDatePicker',array(
+			'name'=>'enddate',
+			// additional javascript options for the date picker plugin
+			'options'=>array(
+					'showAnim'=>'fold',
+			),
+			'htmlOptions'=>array(
+					'id'=>'enddate',
+					'style'=>'height:20px;'
+			),
+			'value'=>$enddate,
+	));
+	?>
+</div>
+		
+<?php CHtml::endForm() ?>
+
+</div>
+
