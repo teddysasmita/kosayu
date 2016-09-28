4ddd79c2e7b8386593709e33d5e258e970f04274
teddys <teddie.sasmita@gmail.com>
2016-09-28 11:49:46 +0700

    Signed-off-by: teddys <teddie.sasmita@gmail.com>

--- protected/modules/guides/views/default/activity.php
+++ protected/modules/guides/views/default/activity.php
@@ -66,7 +66,7 @@ $this->menu=array(
 $provider1 = new CArrayDataProvider($data);
 
 $this->widget('zii.widgets.grid.CGridView',[ 
-		'dataprovider'=>$provider1,
+		'dataProvider'=>$provider1,
 			'columns'=>[
 				[
 					'header'=>'Nomor Stiker',
