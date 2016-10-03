33357b669fe57738bc10f68ffa5b5188ae60963f
teddys <teddie.sasmita@gmail.com>
2016-10-03 11:26:57 +0700

    Signed-off-by: teddys <teddie.sasmita@gmail.com>

--- protected/modules/guides/views/default/printreport1.php
+++ protected/modules/guides/views/default/printreport1.php
@@ -4,50 +4,18 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Cetak Aktivitas Guide</title>
 <style>
-	@page { size: <?php echo $model->paperwidth; ?>mm <?php echo $model->paperheight; ?>mm;
-		margin-bottom: <?php echo $model->paperbotm; ?>mm;
-		margin-top: <?php echo $model->paperbotm; ?>mm; 
-		margin-right: <?php echo $model->papersidem; ?>mm;
-		margin-left: <?php echo $model->papersidem+10; ?>mm;
+	@page { size: <?php echo '215'; ?>mm <?php echo '297'; ?>mm;
+		margin-bottom: <?php echo '10'; ?>mm;
+		margin-top: <?php echo '10'; ?>mm; 
+		margin-right: <?php echo '10'; ?>mm;
+		margin-left: <?php echo '10'; ?>mm;
 		page-break-before: 5mm;
 	}
 	
 	.maintable { 
 		/*margin-left: <?php echo $model->paperwidth - ( 3 * $model->papersidem) - $model->labelwidth - (2 * $model->labelsidem); ?>mm;*/ 	
 		border: none;
-	}
-	
-	.guidetablerow { 
-		height: <?php echo $model->labelheight; ?>mm;
-		margin: <?php echo $model->labelbotm; ?>mm; 
-	}
-	.guidetablecol { 
-		width: <?php echo $model->labelwidth; ?>mm;
-		margin: <?php echo $model->labelsidem; ?>mm;
-		 
-	}
-	.guidetablecell {  
-		border: solid thin;
-		-webkit-print-color-adjust: exact;
-	 }
-	 .cardcontent {
-	 	background-color: yellow;
-	 	height: <?php echo $model->labelheight-4; ?>mm;
-	 	width: <?php echo $model->labelwidth-4; ?>mm;
-	 	margin-top: <?php echo $model->labelbotm; ?>mm;
-	 	margin-bottom: <?php echo $model->labelbotm; ?>mm;
-	 	margin-right: <?php echo $model->labelsidem; ?>mm;
-	 	margin-left: <?php echo $model->labelsidem; ?>mm;
-	 	float: left;
-	 	padding: 2mm;
-	 	border: solid thin;
-	 	page-break-inside: avoid;
-	 }
-	 .emptycontent {
-	 	height: <?php echo $model->labelheight-2; ?>mm;
-	 	width: <?php echo $model->labelwidth-2; ?>mm;
-	 }
-	 
+	}	 
 </style>
 </head>
 <body>
